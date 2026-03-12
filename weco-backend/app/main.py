b398ef3e7aac036a3973e65ab4990aa011611b94
unknown <mi12047@auca.kg>
2026-03-12 19:39:10 +0600

    add vercel domain to cors

--- weco-backend/app/main.py
+++ weco-backend/app/main.py
@@ -19,8 +19,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://fip-171xsibps-uzzanxs-projects.vercel.app",
     ],
+    allow_origin_regex="https://.*vercel.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
